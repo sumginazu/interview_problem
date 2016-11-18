9ef411d66d22a31759a6673faeb78daa1ad606e0
U-NANCY-PC1\Eric <shoup_alex@yahoo.com>
2016-11-17 19:00:22 -0800

    matching complete, adding sampling file to commit

--- nGramSearch.py
+++ nGramSearch.py
@@ -7,6 +7,8 @@ book_inv = {} #book id -> inverted index
 author_books = {} #author -> book ids
 
 def loadData():
+    global num_books, inv_index_token2book
+    global book_info, book_inv, author_books
     db.loadDatabase()
     num_books = db.num_books
     inv_index_token2book = db.inv_index_token2book
@@ -16,4 +18,42 @@ def loadData():
 
 def matchNGram(ngram):
     #ngram: list of tokens
-    return None
+    #returns a list of books that contain the given n-gram
+
+    #get all books that contain every word in the ngram
+    book_ids = set(inv_index_token2book[ngram[0]])
+    for tok in ngram[1:]:
+        book_ids &= set(inv_index_token2book[tok])
+
+    #iterate through these books, noting those which contain this n-gram
+    #(or, if desired, a fuzzy approximation, not implemented here)
+    matches = []
+    for book in book_ids:
+        inv_idxs = [book_inv[book][tok] for tok in ngram]
+        #idx_iter contains pointers we can advance along the inverted indexes.
+        idx_point = [0 for tok in ngram]
+        while True:
+            #check to see if the current token locations represent an n-gram
+            idxs = [inv_idxs[i][idx_point[i]] for i in xrange(len(ngram))]
+            if all([idxs[i+1] - idxs[i] == 1 for i in xrange(len(ngram)-1)]):
+                #if all the indices are sequential, we have a match
+                matches.append(book)
+                break
+            else:
+                #otherwise, we advance the pointer to the lowest index
+                lowest_val = min(idxs)
+                for i in xrange(ngram):
+                    if idxs[i] == lowest_val:
+                        idx_point[i] += 1
+                    #check to avoid going out of bounds
+                    if idx_point[i] > len(inv_idxs[i]):
+                        break
+    return matches
+
+    
+        
+if __name__ == '__main__':
+    loadData()
+    matched_books = matchNGram(["campaign", "brought", "honours"])
+    print [book_info[b] for b in matched_books]
+    
